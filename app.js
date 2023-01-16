3d460ed7809e95ce3cff3cffe200373148d4c8ce
alexisfereira <alexis.fereira@lever.co>
2023-01-16 16:54:56 -0500

    unnesessary files deleted

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-const http = require('http');
-const requesthandler = require('./routes');
+const express = require('express');
+const bodyparser = require('body-parser');
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+const cartRouter = require('./routes/cart');
+const path = require('path');
+const rootdir = require('./util/path');
 
-const server = http.createServer(requesthandler);
+const app = express();
+const port = 3000;
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(3000);
+app.use('/admin', adminRouter);
+app.use(cartRouter);
+app.use(shopRouter);
+app.use('/', (req, res) => {
+  //res.status(404).send('<h1>Page not found.</h1>');
+  res.sendFile(path.join(rootdir, 'views', '404.html'));
+});
 
-/**
- * The following article provides a great overview
- * of available headers and their role:
- *  https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
- */
+app.listen(port, () => {
+  console.log('Listening Port: ' + port + ' :::::');
+});
